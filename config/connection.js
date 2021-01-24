4a2b13b63d39d828d7478c49c16306cf1056f7fc
SAMIA <68030713+lorjoh@users.noreply.github.com>
2021-01-24 15:35:06 -0500

    DBJaw

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,18 @@ var mysql = require("mysql");
 require('dotenv').config();
 var passWord = process.env.MySQL;
 
-var connection = mysql.createConnection({
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else { mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: passWord,
   database: "burgers_db"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
